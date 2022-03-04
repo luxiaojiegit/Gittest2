4c1087f874af48c1471d222d0ac6313986eb43c7
lxj <lxj@qq.com>
2022-03-04 15:11:51 +0800

    m 4

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -4,7 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("123");
-        System.out.println("312");
-
+        System.out.println("111");
     }
 }
